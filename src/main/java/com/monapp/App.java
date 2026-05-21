f13ecb648de50ecbed5020f5ecc68315140b240c
Bryan <nantenainabryan95@gmail.com>
2026-05-21 23:09:02 +0300

    v1 - projet maven initial

---
+++ src/main/java/com/monapp/App.java
@@ -0,0 +1,11 @@
+package com.monapp;
+
+public class App {
+    public static String direBonjour() {
+        return "Bonjour depuis Jenkins !";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(direBonjour());
+    }
+}
